2d88872e9b5a0dd3a6d4d43339a0944b70976703
DenisSoft <bdv@bk.ru>
2017-09-17 20:17:40 +0300

    Implemented the functions of class Guessing Game

---
+++ src/guessing-game.js
@@ -0,0 +1,26 @@
+class GuessingGame {
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currGuess = 0;
+    }
+
+    setRange(_min, _max) {
+        this.min = _min;
+        this.max = _max;
+    }
+
+    guess() {
+        return this.currGuess = Math.round((this.min + this.max) / 2);
+    }
+
+    lower() {
+        this.max = this.currGuess;
+    }
+
+    greater() {
+        this.min = this.currGuess;
+    }
+}
+
+module.exports = GuessingGame;
